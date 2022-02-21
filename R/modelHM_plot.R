a0381028d63ac03edc8769b7774fb69bdeb29c1e
Pierre Le Denmat <pledenmat1@gmail.com>
2022-02-21 14:25:15 +0100

    Update modelHM_plot.R

--- R/modelHM_plot.R
+++ R/modelHM_plot.R
@@ -2,7 +2,7 @@
 #'
 #' @description Returns the sign of each value within a vector. If 0, returns 1 or
 #' -1 with equal probability
-#' @param x 
+#' @param x
 #'
 #' @return
 #' @export
@@ -14,44 +14,44 @@ custom_sign <- function(x){
     if (x[i]==0) {
       coin <- runif(1)
       if (coin<.5) {
-        res[i] <-  1 
+        res[i] <-  1
       }else{
         res[i] <-  -1
       }
     }else{
       res[i] <- sign(x[i])
-    } 
+    }
   }
   return(res)
 }
 
 #' Plot a confidence heat map using the alpha/beta model
 #'
-#' @param alpha 
-#' @param beta 
+#' @param alpha
+#' @param beta
 #' @param type One of 3 possible types of heat map (upper, lower and symmetrical).
 #' Upper (resp. lower) shows the confidence linked to an upper (resp. lower) boundary hit.
 #' Symmetrical shows the symmetrical heat map. Defaults to "upper"
 #' @param plot If TRUE, the heat map is directly plotted. Defaults to TRUE
 #' @param cexax Axis font size
-#' @param cexlab Labels font size 
+#' @param cexlab Labels font size
 #' @param cexmain Title font size
 #' @param dt Time resolution
 #' @param ev_bound Maximum/minimum evidence
 #' @param ev_window Evidence resolution. Should be sufficiently higher than dt
 #' to avoid NULL values within the heat map
 #' @param upperRT Maximum time
-#' 
+#'
 #' @return
 #' @export
 #'
 #' @examples
 create_modelHM <- function(alpha,beta,type="upper",plot=T,cexax=1.5,cexlab=3,cexmain=2.5,
                            dt = .0025, ev_bound = .5, ev_window = .0125, upperRT = 5){
-  
+
   ev_mapping <- seq(-ev_bound,ev_bound,by=ev_window)
   timesteps <- upperRT/dt
-  
+
   hm <- matrix(NA,nrow=timesteps,ncol=length(ev_mapping))
   hm_low <- matrix(NA,nrow=timesteps,ncol=length(ev_mapping))
   hm_up <- matrix(NA,nrow=timesteps,ncol=length(ev_mapping))
@@ -69,7 +69,11 @@ create_modelHM <- function(alpha,beta,type="upper",plot=T,cexax=1.5,cexlab=3,cex
   colMap <- viridis::viridis(length(hm))
   if (plot==T) {
     if (type=="upper") {
-      fields::image.plot(1:dim(hm_up)[1],1:dim(hm_up)[2],hm_up,zlim=c(0,1),col=colMap,ylab="",xlab='',legend.shrink=.25,main=paste('alpha = ',round(alpha,3),'; beta =',round(beta,3)),axes=F,cex.main=cexmain,axis.args=list(at=seq(0,1,.5),labels=seq(0,1,.5),cex.axis=cexax))
+      fields::image.plot(1:dim(hm_up)[1],1:dim(hm_up)[2],hm_up,zlim=c(0,1),
+                         col=colMap,ylab="",xlab='',legend.shrink=.25,
+                         main=paste('alpha = ',round(alpha,3),'; beta =',round(beta,3)),
+                         axes=F,cex.main=cexmain,
+                         axis.args=list(at=seq(0,1,.5),labels=seq(0,1,.5),cex.axis=cexax))
       mtext("Evidence",2,at=dim(hm_up)[2]/2,line=2,cex=cexlab);
       mtext("Time (s)",1,at=dim(hm_up)[1]/2,line=2,cex=cexlab)
       # axis(1,at=c(1,timesteps),labels=c(0,upperRT),cex.axis=cexax);
@@ -81,7 +85,7 @@ create_modelHM <- function(alpha,beta,type="upper",plot=T,cexax=1.5,cexlab=3,cex
       mtext("Time (s)",1,at=dim(hm_low)[1]/2,line=2,cex=cexlab)
       # axis(1,at=c(1,timesteps),labels=c(0,upperRT),cex.axis=cexax,cex.lab=2)
       # axis(2,at=c(1,length(ev_mapping)),labels=c(-ev_bound,ev_bound))
-      
+
     }else if(type=="symmetrical"){ #
       fields::image.plot(1:dim(hm)[1],1:dim(hm)[2],hm,zlim=c(0,1),col=colMap,ylab="Evidence",xlab='time (s)',main=paste('alpha = ',round(alpha,3),'; beta = ',round(beta,3)),col=colMap,axes=F)
       mtext("Evidence",2,at=dim(hm)[2]/2,line=2,cex=cexlab);
@@ -98,6 +102,6 @@ create_modelHM <- function(alpha,beta,type="upper",plot=T,cexax=1.5,cexlab=3,cex
         return(hm)
     }
   }
-}  
+}
 
 
